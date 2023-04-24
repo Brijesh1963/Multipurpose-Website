a884a0887b4b5ad2e4f9968fcc78fd6130c43b3a
Brijesh Pramanik <98037970+Brijesh1963@users.noreply.github.com>
2023-04-24 15:11:09 +0530

    all files

---
+++ photo.js
@@ -0,0 +1,36 @@
+// const mobile_nav = document.querySelector(".mobile-navbar-btn");
+// const nav_header = document.querySelector(".header");
+
+// const toggleNavbar = () => {
+//   // alert("Plz Subscribe ");
+//   nav_header.classList.toggle("active");
+// };
+
+// mobile_nav.addEventListener("click", () => toggleNavbar());
+const selectImage = document.querySelector('.select-image');
+const inputFile = document.querySelector('#file');
+const imgArea = document.querySelector('.img-area');
+
+selectImage.addEventListener('click', function () {
+	inputFile.click();
+})
+
+inputFile.addEventListener('change', function () {
+	const image = this.files[0] 
+	if(image.size < 2000000) {
+		const reader = new FileReader();
+		reader.onload = ()=> {
+			const allImg = imgArea.querySelectorAll('img');  
+			allImg.forEach(item=> item.remove());
+			const imgUrl = reader.result;
+			const img = document.createElement('img');
+			img.src = imgUrl;
+			imgArea.appendChild(img);
+			imgArea.classList.add('active');
+			imgArea.dataset.img = image.name;
+		}
+		reader.readAsDataURL(image);
+	} else {
+		alert("Image size more than 2MB");
+	}
+})
